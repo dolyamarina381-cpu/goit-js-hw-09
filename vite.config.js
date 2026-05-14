c541ff204a74679845f2625fdffa254567b5204e
unknown <dolyamarina@gmail.com>
2026-05-14 20:37:56 +0300

    fix structure

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,13 @@ const rootDir = fileURLToPath(new URL('.', import.meta.url));
 export default defineConfig({
   base: '/goit-js-hw-09/',
   root: rootDir,
-  resolve: {
-    alias: {
-      simplelightbox: resolve(rootDir, 'node_modules/simplelightbox'),
-    },
-  },
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(rootDir, 'index.html'),
-        gallery: resolve(rootDir, '1-gallery.html'),
-        form: resolve(rootDir, '2-form.html'),
+        gallery: resolve(rootDir, 'src/1-gallery.html'),
+        form: resolve(rootDir, 'src/2-form.html'),
       },
     },
   },
