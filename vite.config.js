a37cdb464b08c88cac720e61b555f09137c78c2a
unknown <dolyamarina@gmail.com>
2026-05-14 21:42:59 +0300

    chore: apply all current changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   base: '/goit-js-hw-09/',
+  root: rootDir,
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(rootDir, 'index.html'),
+        gallery: resolve(rootDir, 'src/1-gallery.html'),
+        form: resolve(rootDir, 'src/2-form.html'),
+      },
+    },
+  },
 });
