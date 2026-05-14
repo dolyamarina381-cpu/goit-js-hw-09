889c78ff2d0f5b4834dbbc027b7a6bf6e27337b9
unknown <dolyamarina@gmail.com>
2026-05-14 22:23:05 +0300

    fix: sync local changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-const root = resolve(__dirname);
+import { fileURLToPath } from 'node:url';
+const rootDir = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  base: '/goit-js-hw-09/',
+  root: rootDir,
   build: {
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
-        gallery: resolve(root, '1-gallery.html'),
-        form: resolve(root, '2-form.html'),
+        main: resolve(rootDir, 'index.html'),
+        gallery: resolve(rootDir, '1-gallery.html'),
+        form: resolve(rootDir, '2-form.html'),
       },
     },
   },
